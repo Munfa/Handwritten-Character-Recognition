87b81abca5cea8b32d3f84b12c16fd0d22d2f8e2
SabbirAhmedSany <71925082+SabbirAhmedSany@users.noreply.github.com>
2020-12-15 19:42:14 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -4,12 +4,13 @@
 #import necessary packages
 import numpy as np
 import pandas as pd
+import cv2
 import tensorflow as tf
-import matplotlib as plt
-from tf.keras.datasets import mnist
+import matplotlib.pyplot as plt
 
+mnist = tf.keras.datasets.mnist
 #load A-Z dataset
-datasetPath = pd.read_csv(r'A_Z Handwritten Data.csv')
+
 def load_az_dataset(datasetPath):
   data = []
   labels = []
@@ -19,7 +20,7 @@ def load_az_dataset(datasetPath):
     #split label and image from the row
     row = row.split(",")
     label = int(row[0])
-    image = np.array([int(x) for x in row[1:]], dtype="unit8")
+    image = np.array([int(x) for x in row[1:]], dtype=np.uint8)
 
     #reshaping images into 28X28 matrix
     image = image.reshape((28,28))
@@ -33,13 +34,23 @@ def load_az_dataset(datasetPath):
 
     return (data, labels)
 
-#load MNIST handwritten digit dataset
-def load_mnist_dataset():
-  ((trainData, trainLabels), (testData, testLabels)) = mnist.load_data()
-  data = np.vstack([trainData, testData])
-  labels = np.hstack([trainLabels, testLabels])
+  # load MNIST handwritten digit dataset
+  def load_mnist_dataset():
+      (trainData, trainLabels), (testData, testLabels) = mnist.load_data()
+      trainData = tf.keras.utils.normalize(trainData, axis=1)
+      testData = tf.keras.utils.normalize(testData, axis=1)
+      data = np.vstack([trainData, testData])
+      labels = np.hstack([trainLabels, testLabels])
 
-  return (data, labels)
+      return (data, labels)
 
+  # loading both datasets and combining them into one
+  azPath = "A_Z Handwritten Data.csv"
+  (azData, azLabels) = load_az_dataset(azPath)
+  (digitData, digitLabels) = load_mnist_dataset()
 
+  # as MNIST has labels 0-9, we are going to add 10 to ever A-Z labels so that they are not labeled as digits
+  azLabels += 10
 
+  data = np.vstack([azData, digitData])
+  labels = np.hstack([azLabels, digitLabels])
