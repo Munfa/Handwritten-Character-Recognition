52d7cd04b666686933cdce8eeb98f8c152b6bed0
Munfa <72749554+Munfa@users.noreply.github.com>
2020-12-15 19:56:06 +0600

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 mnist = tf.keras.datasets.mnist
-#load A-Z dataset
 
+#load A-Z dataset
 def load_az_dataset(datasetPath):
   data = []
   labels = []
@@ -29,28 +29,28 @@ def load_az_dataset(datasetPath):
     data.append(image)
     labels.append(label)
 
-    data = np.array(data, dtype="float32")
-    labels = np.array(labels, dtype="int")
+  data = np.array(data, dtype="float32")
+  labels = np.array(labels, dtype="int")
+
+  return (data, labels)
+
+ # load MNIST handwritten digit dataset
+ def load_mnist_dataset():
+    (trainData, trainLabels), (testData, testLabels) = mnist.load_data()
+    trainData = tf.keras.utils.normalize(trainData, axis=1)
+    testData = tf.keras.utils.normalize(testData, axis=1)
+    data = np.vstack([trainData, testData])
+    labels = np.hstack([trainLabels, testLabels])
 
     return (data, labels)
 
-  # load MNIST handwritten digit dataset
-  def load_mnist_dataset():
-      (trainData, trainLabels), (testData, testLabels) = mnist.load_data()
-      trainData = tf.keras.utils.normalize(trainData, axis=1)
-      testData = tf.keras.utils.normalize(testData, axis=1)
-      data = np.vstack([trainData, testData])
-      labels = np.hstack([trainLabels, testLabels])
+# loading both datasets and combining them into one
+azPath = "A_Z Handwritten Data.csv"
+(azData, azLabels) = load_az_dataset(azPath)
+(digitData, digitLabels) = load_mnist_dataset()
 
-      return (data, labels)
+# as MNIST has labels 0-9, we are going to add 10 to ever A-Z labels so that they are not labeled as digits
+azLabels += 10
 
-  # loading both datasets and combining them into one
-  azPath = "A_Z Handwritten Data.csv"
-  (azData, azLabels) = load_az_dataset(azPath)
-  (digitData, digitLabels) = load_mnist_dataset()
-
-  # as MNIST has labels 0-9, we are going to add 10 to ever A-Z labels so that they are not labeled as digits
-  azLabels += 10
-
-  data = np.vstack([azData, digitData])
-  labels = np.hstack([azLabels, digitLabels])
+data = np.vstack([azData, digitData])
+labels = np.hstack([azLabels, digitLabels])
