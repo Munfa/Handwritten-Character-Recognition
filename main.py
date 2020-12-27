fcb2919a27ff111448309d95be770acb7dfa79a1
SabbirAhmedSany <71925082+SabbirAhmedSany@users.noreply.github.com>
2020-12-27 18:43:36 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,45 @@
 #OCR with Keras, TensorFlow and Deep Learning
-#Image input
 
 #import necessary packages
 import numpy as np
-import pandas as pd
 import cv2
-import tensorflow as tf
+import pandas as pd
+import math
 import matplotlib.pyplot as plt
+import matplotlib
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.optimizers import Adam
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import classification_report
 
 mnist = tf.keras.datasets.mnist
 
-#load A-Z dataset
+# #load A-Z dataset
+
 def load_az_dataset(datasetPath):
   data = []
   labels = []
+  import math
+  dataset = pd.read_csv(datasetPath)
 
   #loop over the rows of the A-Z dataset
-  for row in open(datasetPath):
-    #split label and image from the row
-    row = row.split(",")
-    label = int(row[0])
-    image = np.array([int(x) for x in row[1:]], dtype=np.uint8)
+  for row in dataset:
+      #split label and image from the row
+      row = row.split(",")
+      
+      label = math.floor(float(row[0]))
+      image = np.array([int(x) for x in row[1:]], dtype="uint8")
 
-    #reshaping images into 28X28 matrix
-    image = image.reshape((28,28))
-
-    #update list of data and labels
-    data.append(image)
-    labels.append(label)
+      #reshaping images into 28X28 matrix
+      image = np.arange(784).reshape(28,28)
+      
+      #update list of data and labels
+      data.append(image)
+      labels.append(label)
 
   data = np.array(data, dtype="float32")
-  labels = np.array(labels, dtype="int")
+  labels = np.array(labels, dtype=None)  
 
   return (data, labels)
 
@@ -43,8 +52,7 @@ def load_az_dataset(datasetPath):
     labels = np.hstack([trainLabels, testLabels])
 
     return (data, labels)
-
-# loading both datasets and combining them into one
+#loading both datasets and combining them into one
 azPath = "A_Z Handwritten Data.csv"
 (azData, azLabels) = load_az_dataset(azPath)
 (digitData, digitLabels) = load_mnist_dataset()
@@ -54,3 +62,10 @@ azLabels += 10
 
 data = np.vstack([azData, digitData])
 labels = np.hstack([azLabels, digitLabels])
+
+data = [cv2.resize(image, (28, 28)) for image in data]
+data = np.array(data, dtype="float32")
+
+#add a channel dimension to every image in the dataset and normalize them
+data = np.expand_dims(data, axis=-1)
+data /= 255.0
